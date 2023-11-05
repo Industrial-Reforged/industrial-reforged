f2193609c5b5f33ad870ccb64a287bb233d7d3fc
thepigcat <benpospo@gmail.com>
2023-11-05 22:23:03 +0100

    Firebox multi gui for the coil

--- src/main/java/com/indref/industrial_reforged/content/blocks/CoilBlock.java
+++ src/main/java/com/indref/industrial_reforged/content/blocks/CoilBlock.java
@@ -5,13 +5,22 @@ import com.indref.industrial_reforged.api.multiblocks.IMultiBlockController;
 import com.indref.industrial_reforged.api.multiblocks.IMultiblock;
 import com.indref.industrial_reforged.content.blockentities.FireboxBlockEntity;
 import com.indref.industrial_reforged.content.multiblocks.FireBoxMultiblock;
+import com.indref.industrial_reforged.util.BlockUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
+import net.neoforged.neoforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class CoilBlock extends BaseEntityBlock implements IMultiBlockController, IWrenchable {
@@ -38,5 +47,18 @@ public class CoilBlock extends BaseEntityBlock implements IMultiBlockController,
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new FireboxBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        if (!level.isClientSide()) {
+            if (!blockState.getValue(FireBoxMultiblock.FIREBOX_PART).equals(FireBoxMultiblock.PartIndex.UNFORMED)) {
+                BlockEntity fireBoxBlockEntity = level.getBlockEntity(blockPos);
+                NetworkHooks.openScreen(((ServerPlayer) player), (FireboxBlockEntity) fireBoxBlockEntity, blockPos);
+                return InteractionResult.SUCCESS;
+            }
+            return InteractionResult.FAIL;
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
     }
 }
