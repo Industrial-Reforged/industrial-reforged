258268f3311841aa09a14b9c30e109d88dcf31ef
thepigcat <benpospo@gmail.com>
2023-11-18 00:25:47 +0100

    fluidcell placement & pickup

--- src/main/java/com/indref/industrial_reforged/registries/items/storage/FluidCellItem.java
+++ src/main/java/com/indref/industrial_reforged/registries/items/storage/FluidCellItem.java
@@ -1,44 +1,38 @@
 package com.indref.industrial_reforged.registries.items.storage;
 
 import com.indref.industrial_reforged.api.items.SimpleFluidItem;
+import com.indref.industrial_reforged.api.items.container.IFluidItem;
 import com.indref.industrial_reforged.registries.IRItems;
+import com.indref.industrial_reforged.util.ItemUtils;
 import net.minecraft.ChatFormatting;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentContents;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BucketPickup;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.neoforged.neoforge.common.SoundAction;
 import net.neoforged.neoforge.common.capabilities.Capabilities;
-import net.neoforged.neoforge.fluids.FluidStack;
-import net.neoforged.neoforge.fluids.capability.IFluidHandlerItem;
-import net.neoforged.neoforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-import static net.neoforged.neoforge.event.EventHooks.onBucketUse;
+import static net.minecraft.world.level.block.LiquidBlock.LEVEL;
 
 public class FluidCellItem extends SimpleFluidItem {
     private final int capacity;
@@ -50,73 +44,57 @@ public class FluidCellItem extends SimpleFluidItem {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
-        BlockHitResult hit = getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY);
-        InteractionResultHolder<ItemStack> ret = onBucketUse(player, level, stack, hit);
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack handItem = ItemUtils.itemStackFromInteractionHand(interactionHand, player);
+        BlockHitResult blockhitresult = getPlayerPOVHitResult(
+                level, player, this.fluid == Fluids.EMPTY ? ClipContext.Fluid.SOURCE_ONLY : ClipContext.Fluid.NONE
+        );
+        if (blockhitresult.getType() == HitResult.Type.MISS) {
+            return InteractionResultHolder.pass(handItem);
+        } else if (blockhitresult.getType() != HitResult.Type.BLOCK) {
+            return InteractionResultHolder.pass(handItem);
+        } else {
+            if (handItem.getItem() instanceof IFluidItem fluidItem) {
+                BlockPos blockpos = blockhitresult.getBlockPos();
+                Direction direction = blockhitresult.getDirection();
+                BlockPos blockpos1 = blockpos.relative(direction);
+                Fluid fluid1 = fluidItem.getFluidHandler(handItem).getFluidInTank(0).getFluid();
+                if (fluid1 == Fluids.EMPTY) {
+                    BlockState blockstate1 = level.getBlockState(blockpos);
+                    Block block1 = blockstate1.getBlock();
+                    if (block1 instanceof BucketPickup bucketpickup) {
+                        ItemStack itemstack2 = getFilledResult(level, blockpos, blockstate1);
+                        if (!itemstack2.isEmpty()) {
+                            bucketpickup.getPickupSound(blockstate1).ifPresent(p_150709_ -> player.playSound(p_150709_, 1.0F, 1.0F));
+                            ItemStack itemstack1 = net.minecraft.world.item.ItemUtils.createFilledResult(handItem, player, itemstack2);
 
-        if (ret != null) {
-            return ret;
-        } else if (hit.getType() == HitResult.Type.MISS) {
-            return InteractionResultHolder.pass(stack);
-        } else if (hit.getType() != HitResult.Type.BLOCK) {
-            return InteractionResultHolder.pass(stack);
-        } else if (getFluid() != Fluids.EMPTY) {
-            return InteractionResultHolder.fail(stack);
-        }
-
-        BlockPos pos = hit.getBlockPos();
-        Direction direction = hit.getDirection();
-        BlockPos pos1 = pos.relative(direction);
-
-        if (level.mayInteract(player, pos) && player.mayUseItemAt(pos1, direction, stack)) {
-            BlockState state = level.getBlockState(pos);
-            IFluidHandlerItem cap = stack.getCapability(Capabilities.FLUID_HANDLER_ITEM).orElseThrow(NullPointerException::new);
-            if (cap.getFluidInTank(0).isEmpty()) {
-                if (state.getBlock() instanceof LiquidBlock liquidBlock) {
-                    if (state.getValue(BlockStateProperties.LEVEL) == 0) {
-                        level.setBlock(pos, Blocks.AIR.defaultBlockState(), 11);
-                    }
-
-                    Fluid fluid = liquidBlock.getFluid();
-                    if (fluid != Fluids.EMPTY) {
-                        player.awardStat(Stats.ITEM_USED.get(this));
-                        SoundEvent soundevent = fluid.getFluidType().getSound(SoundAction.get("fill"));
-
-                        if (soundevent == null) {
-                            soundevent = fluid.isSame(Fluids.LAVA) ? SoundEvents.BUCKET_FILL_LAVA : SoundEvents.BUCKET_FILL;
+                            return InteractionResultHolder.success(itemstack1);
                         }
-
-                        player.playSound(soundevent, 1F, 1F);
-
-                        ItemStack newStack = new ItemStack(this);
-                        IFluidHandlerItem newCap = newStack.getCapability(Capabilities.FLUID_HANDLER_ITEM).orElseThrow(NullPointerException::new);
-                        newCap.fill(new FluidStack(fluid, 1000), IFluidHandlerItem.FluidAction.EXECUTE);
-                        this.fluid = fluid;
-
-                        if (!level.isClientSide()) {
-                            CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, new ItemStack(fluid.getBucket()));
-                        }
-
-                        return InteractionResultHolder.sidedSuccess(ItemUtils.createFilledResult(stack, player, newStack), level.isClientSide());
                     }
-                }
-
-            } else {
-                if (!(state.getBlock() instanceof LiquidBlock) && !cap.getFluidInTank(0).getFluid().getFluidType().isVaporizedOnPlacement(level, pos, cap.getFluidInTank(0)) && state.canBeReplaced(fluid)) {
-                    level.setBlock(pos1, cap.getFluidInTank(0).getFluid().defaultFluidState().createLegacyBlock(), 11);
-                    if (!player.isCreative()) {
-                        this.fluid = Fluids.EMPTY;
-                        stack.shrink(1);
-                        ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(IRItems.FLUID_CELL.get()));
+                    return InteractionResultHolder.fail(handItem);
+                } else {
+                    if (!(level.getBlockState(blockpos).getBlock() instanceof LiquidBlock)
+                            && !fluid1.getFluidType().isVaporizedOnPlacement(level, blockpos, fluidItem.getFluidHandler(handItem).getFluidInTank(0))) {
+                        level.setBlock(blockpos1, fluid1.defaultFluidState().createLegacyBlock(), 11);
+                        return InteractionResultHolder.success(handItem);
                     }
-                    return InteractionResultHolder.success(stack);
                 }
             }
         }
+        return InteractionResultHolder.fail(handItem);
+    }
 
-
-        return InteractionResultHolder.fail(stack);
+    public ItemStack getFilledResult(LevelAccessor level, BlockPos blockPos, BlockState blockState) {
+        if (blockState.getValue(LEVEL) == 0) {
+            if (blockState.getBlock() instanceof LiquidBlock liquidBlock) {
+                level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), 11);
+                ItemStack stack = new ItemStack(IRItems.FLUID_CELL.get());
+                IFluidItem fluidItem = ItemUtils.getFluidItem(stack);
+                fluidItem.tryFill(liquidBlock.getFluid().getSource(), 1000, stack);
+                return stack;
+            }
+        }
+        return ItemStack.EMPTY;
     }
 
     @Override
